e6831bb884f3e0e93552371c6ef1ab07b7b5cc77
Jose Balado <JoseBalado@users.noreply.github.com>
2019-04-11 12:02:30 +0100

    Show benchmark

--- server.mjs
+++ server.mjs
@@ -2,4 +2,11 @@ import { Worker } from 'worker_threads'
 
 const worker = new Worker('./worker-thread.mjs')
 
-worker.on('message', msg => console.log(msg))
+const NS_PER_SEC = 1e9
+const time = process.hrtime()
+
+worker.on('message', msg => {
+  console.log(msg[0])
+  const diff = process.hrtime(time)
+  console.log(`Benchmark took ${diff[0] + diff[1] / NS_PER_SEC} seconds`)
+})
