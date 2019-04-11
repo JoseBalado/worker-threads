0bbe05858864b1753ac5fdeaee4131290d482e11
Jose Balado <JoseBalado@users.noreply.github.com>
2019-04-11 12:47:00 +0100

    define end variable with const

--- worker-thread.mjs
+++ worker-thread.mjs
@@ -4,7 +4,7 @@ const primes = []
 
 const generatePrimes = (start, range) => {
   let isPrime = true
-  let end = start + range
+  const end = start + range
   for (let i = start; i < end; i++) {
     for (let j = start; j < Math.sqrt(end); j++) {
       if (i !== j && i % j === 0) {
