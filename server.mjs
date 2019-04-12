015bd58f00b8de59f3b2cd347251d98041eab626
Jose Balado <JoseBalado@users.noreply.github.com>
2019-04-12 17:02:34 +0100

    Change workersPoll from array to object

--- server.mjs
+++ server.mjs
@@ -3,38 +3,38 @@ import os from 'os'
 
 const interval = []
 let index = 0
-while (index < 1000000000) {
-  interval.push({ start: index, end: index += 99999999 })
+while (index < 100000000) {
+  interval.push({ start: index, end: index += 9999999 })
   index++
 }
 
 console.log('interval', interval)
 
-const workersPoll = []
+const workersPoll = {}
 for (let index = 0; index < (os.cpus().length - 1); index++) {
-  workersPoll.push(new Worker('./worker-thread.mjs'))
+  workersPoll[`id${index}`] = { idle: true, worker: new Worker('./worker-thread.mjs') }
 }
 
-workersPoll[0].postMessage(interval.pop())
+workersPoll.id0.worker.postMessage(interval.pop())
 
 const NS_PER_SEC = 1e9
 const time = process.hrtime()
 
-workersPoll[0].on('message', msg => {
+workersPoll.id0.worker.on('message', msg => {
   // console.log('msg: ', msg)
   const diff = process.hrtime(time)
 
   console.log('il', interval.length)
   if(interval.length === 0) {
-    interval.length || workersPoll[0].terminate()
+    interval.length || workersPoll.id0.worker.terminate()
     console.log(`Benchmark took ${diff[0] + diff[1] / NS_PER_SEC} seconds`)
   }
-  interval.length && workersPoll[0].postMessage(interval.pop())
+  interval.length && workersPoll.id0.worker.postMessage(interval.pop())
 })
 
-workersPoll[0].on('exit', param => {
+workersPoll.id0.worker.on('exit', param => {
   console.log('worker exits', param)
 
   // Needed to finish with node application
-  workersPoll.map(worker => worker.terminate())
+  Object.keys(workersPoll).forEach(id => workersPoll[id].worker.terminate())
 })
